f4a513a6aa9d30a1fda182c515eb12ee5db0327f
Synxndh <117395956+Synxndh@users.noreply.github.com>
2023-05-12 23:56:59 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import * as express from 'express';
 import router from './routes';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
