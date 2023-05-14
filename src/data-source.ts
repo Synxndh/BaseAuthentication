7122b234dc48ef5ff09c900529b5c5c9aaf9ea91
Synxndh <117395956+Synxndh@users.noreply.github.com>
2023-05-14 11:19:40 +0700

    update

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,7 +6,7 @@ import { RefreshToken } from './entity/RefreshToken';
 export const AppDataSource = new DataSource({
     type: 'postgres',
     host: 'localhost',
-    port: 5432,
+    // port: 5432,
     username: 'postgres',
     password: '123456',
     database: 'jwt-authorization',
